b64b76eaad333886f69555156c69be5df6ad8507
Arka Nayan <itsarkanayan@gmail.com>
2017-02-14 21:03:13 +0530

    Check if alphabet is empty

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -147,6 +147,11 @@ bool DbManager::insertPoint(const Point &point,const int position, const int str
 
 bool DbManager::insertAlphabet(const Alphabet &alphabet, const int user_id, const int sample_text_id) {
 
+    if(alphabet.size() == 0) {
+        qDebug() << "Alphabet is empty! write something";
+        return false;
+    }
+
     bool success = true;
 
     QSqlDatabase db = QSqlDatabase::database();
